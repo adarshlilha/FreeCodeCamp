2a27d25dc7344755a97231d097ff456b6dfdc583
Adarsh Lilha <adarshlilha@gmail.com>
2017-11-01 11:51:52 +0530

    Fixed Show Results Function

--- Wikipedia Viewer/script.js
+++ Wikipedia Viewer/script.js
@@ -1,15 +1,13 @@
-function clearSection(){
+function getApiResponse(){
+	//Clear existing results(if any)
 	var searchRows = document.getElementById('searchRows');
 	searchRows.innerHTML = "";
-	
-	setTimeout(getApiResponse,2000);
-}
 
-function getApiResponse(){
+	//Craft URL
 	var userInput = document.querySelector('input').value;
 	var url = 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=' +userInput + '&prop=info&imlimit=10&format=json&$wgCapitalLinks=false';
-	console.log(url);
-
+	
+	//Make API call and pass data to showApiData function
 	fetch(url).then(function(data){
 		return data.json();
 	}).then(function(myBlob){
@@ -17,13 +15,14 @@ function getApiResponse(){
 	});
 }
 
-
 //Show Response Data
-var results = [];
 function showApiData(apiData){
+	var results = [];
 	var j=0;
+	console.log(apiData);	
+	var length = apiData[1].length;
 	//Clear the result rows
-	while (j<10){
+	while (j<length){
 		var row = [];
 		for (var i=1;i<=3;i++){
 			row.push(apiData[i][j]);
@@ -36,24 +35,25 @@ function showApiData(apiData){
 		//Appending the section to parent class
 		var searchRows = document.getElementById('searchRows');
 		searchRows.innerHTML += loader;
-		// loader.appendTo('#searchRows');
 	}
 	var totalRows = document.querySelectorAll('.searchRow');
 	for (var i=0;i<totalRows.length;i++){
-		$(totalRows[i].childNodes[0]).html(results[i][0]);
-		$(totalRows[i].childNodes[1]).html(results[i][1]);
-		$(totalRows[i].childNodes[2]).attr("href",results[i][2]);
-		$(totalRows[i].childNodes[2]).text("Go to Article");
+		(totalRows[i].childNodes[0]).innerHTML = results[i][0];
+		(totalRows[i].childNodes[1]).innerHTML = results[i][1];
+		(totalRows[i].childNodes[2]).href = results[i][2];
+		(totalRows[i].childNodes[2]).innerText = "Go to Article";
 	}
-	$('footer').css({'display':'block'});
+	document.querySelector('footer').style.display = "block";
 }
 
+//Call getApiResponse on button click
 var searchBtn = document.querySelector('button');
-searchBtn.addEventListener('click',clearSection);
+searchBtn.addEventListener('click',getApiResponse);
 
+//Call getApiResponse on return key press
 var input = document.querySelector('input');
 input.addEventListener('keyup',function(e){
 	if (e.keyCode === 13){
-		clearSection();
+		getApiResponse();
 	}
 });
